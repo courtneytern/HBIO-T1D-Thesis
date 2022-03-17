1a599516375624c4f1bbf569fb829b3973d90424
Courtney Tern <catern19@gmail.com>
2022-03-16 22:53:54 -0400

    Create plot_omnibus_pcs.R
    make sploms color coded by sex

---
+++ plot_omnibus_pcs.R
@@ -0,0 +1,31 @@
+# Plot scatterplot for all combinations of PCs
+# for AFR and AMR omnibus data
+
+# module load gcc/7.1.0  openmpi/3.1.4  R
+# R
+
+library(lattice)
+
+setwd("/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/omnibus")
+# setwd("~/Downloads")
+## COLOR CODE BY CASE-CONTROL AND SEX
+AFR<- fread("./T1DGC_HCE_AFR.OMNIBUS.pcs")
+  AFR$FID<- as.character(AFR$FID)
+AFR_sex<- fread("./T1DGC_HCE_AFR.OMNIBUS.sex")
+withSexAFR<- merge(AFR,AFR_sex,by.x=c("FID","IID"),by.y=c("V1","V2"),all.x=T)
+# color by sex
+AFRPlot<- splom(withSexAFR[,3:12], groups=withSexAMR$V3, auto.key=list(space="right"),
+                main="AFR Omnibus PC Associations", xlab="PCs 1-10",ylab="PCs 1-10")
+
+AMR<- fread("./T1DGC_HCE_AMR.OMNIBUS.pcs")
+  AMR$FID<- as.character(AMR$FID)
+AMR_sex<- fread("./T1DGC_HCE_AMR.OMNIBUS.sex")
+withSexAMR<- merge(AMR,AMR_sex,by.x=c("FID","IID"),by.y=c("V1","V2"),all.x=T)
+# color by sex
+AMRPlot<- splom(withSexAMR[,3:12], groups=withSexAMR$V3, auto.key=list(space="right"), 
+                main="AMR Omnibus PC Associations", xlab="PCs 1-10",ylab="PCs 1-10")
+
+pdf("omnibusPlots_AFR_AMR_bysex.pdf")
+  AFRPlot
+  AMRPlot
+dev.off()
