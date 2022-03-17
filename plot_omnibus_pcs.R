1b2d99799065c1c1769879b0d45602a5545c33db
Courtney Tern <catern19@gmail.com>
2022-03-16 23:03:25 -0400

    update graph titles and paths

--- plot_omnibus_pcs.R
+++ plot_omnibus_pcs.R
@@ -5,6 +5,7 @@
 # R
 
 library(lattice)
+library(data.table)
 
 setwd("/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/omnibus")
 # setwd("~/Downloads")
@@ -15,7 +16,7 @@ AFR_sex<- fread("./T1DGC_HCE_AFR.OMNIBUS.sex")
 withSexAFR<- merge(AFR,AFR_sex,by.x=c("FID","IID"),by.y=c("V1","V2"),all.x=T)
 # color by sex
 AFRPlot<- splom(withSexAFR[,3:12], groups=withSexAMR$V3, auto.key=list(space="right"),
-                main="AFR Omnibus PC Associations", xlab="PCs 1-10",ylab="PCs 1-10")
+                main="AFR Omnibus PC Associations By Sex", xlab="PCs 1-10",ylab="PCs 1-10")
 
 AMR<- fread("./T1DGC_HCE_AMR.OMNIBUS.pcs")
   AMR$FID<- as.character(AMR$FID)
@@ -23,9 +24,9 @@ AMR_sex<- fread("./T1DGC_HCE_AMR.OMNIBUS.sex")
 withSexAMR<- merge(AMR,AMR_sex,by.x=c("FID","IID"),by.y=c("V1","V2"),all.x=T)
 # color by sex
 AMRPlot<- splom(withSexAMR[,3:12], groups=withSexAMR$V3, auto.key=list(space="right"), 
-                main="AMR Omnibus PC Associations", xlab="PCs 1-10",ylab="PCs 1-10")
+                main="AMR Omnibus PC Associations By Sex", xlab="PCs 1-10",ylab="PCs 1-10")
 
-pdf("omnibusPlots_AFR_AMR_bysex.pdf")
+pdf("/nv/vol185/T1DGC/USERS/cat7ep/project/figures/omnibusPlots_AFR_AMR_bysex.pdf")
   AFRPlot
   AMRPlot
 dev.off()
