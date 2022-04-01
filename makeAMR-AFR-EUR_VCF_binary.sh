d637ca59cbefbc703903d708de2bc9d27d72cd31
Courtney Tern <catern19@gmail.com>
2022-04-01 00:31:27 -0400

    rename

--- makeAMR-AFR-EUR_VCF_binary.sh
+++ makeAMR-AFR-EUR_VCF_binary.sh
@@ -8,8 +8,10 @@ module load plink
 wd="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6"
 AFR_samps="T1DGC_HCE_AFR_FINAL_sample_list.txt"
 AMR_samps="T1DGC_HCE_AMR_FINAL_sample_list.txt"
+EUR_samps="tmp_5_fam_EUR_cc_unrelated.txt"
 AFR_out="T1DGC_HCE_AFR-only"
 AMR_out="T1DGC_HCE_AMR-only"
+EUR_out="T1DGC_HCE_unrelated_EUR"
 
 cd $wd
 
@@ -22,3 +24,6 @@ plink --vcf $wd/filtered021322.recode.vcf.gz \
 # make binary files
 plink --vcf $wd/${AFR_out}.vcf --make-bed --out $AFR_out
 plink --vcf $wd/${AMR_out}.vcf --make-bed --out $AMR_out
+
+
+plink --bfile T1DGC_HCE_updated_fam --keep $wd/$EUR_samps --make-bed --out $EUR_out
