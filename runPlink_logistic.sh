5892a4770dbf246abc2107d5bd26b63fbeea2df0
Courtney Tern <catern19@gmail.com>
2022-04-01 00:31:14 -0400

    add EUR logistic

--- runPlink_logistic.sh
+++ runPlink_logistic.sh
@@ -8,28 +8,30 @@ module load plink
 ######################
 ## edit covar file ###
 ######################
-cd /nv/vol185/T1DGC/USERS/cat7ep/data
-# print first col twice to match fam ID and individ ID
-awk '{ print $2,$2,$3,$4,$5,$6 }' < T1DGC_HCE_AFR_cov.txt > T1DGC_HCE_AFR_FINAL_cov.txt
+# cd /nv/vol185/T1DGC/USERS/cat7ep/data
+# # print first col twice to match fam ID and individ ID
+# awk '{ print $2,$2,$3,$4,$5,$6 }' < T1DGC_HCE_AFR_cov.txt > T1DGC_HCE_AFR_FINAL_cov.txt
 
-#################################
-## run plink on imputed files ###
-#################################
+# remove sex/cc status from EUR PCs. do not print header
+awk '{ if( NR!=1 ) {print $1,$2,$7,$8,$9,$10} }' < T1DGC_HCE_pc_EUR.txt > T1DGC_HCE_EUR_FINAL_cov.txt
+
+###############################
+## run plink on AFR and AMR ###
+###############################
 # run plink on the imputed files. will give the SNP associations
 ## run with keep and covar
 
 filePath="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6"
 # 1KG imputed file path:
 ## filePath="/nv/vol185/T1DGC/USERS/cat7ep/data/1KG_imputed_032321"
-
-# unzip the vcf to run plink
-vcfFileName="chr6.dose.vcf"
+vcfFileName="filtered032722.vcf.gz"
 
 filterPath="/nv/vol185/T1DGC/USERS/cat7ep/data"
 phenoFile="T1DGC_HCE-2021-10-07_CT.phe"
-keepFile="T1DGC_HCE_AMR_FINAL_sample_list.txt"
-covarFile="T1DGC_HCE_AMR_FINAL_cov.txt"
-outStem="multiethnic.AMR_only.v3"
+keepFile="T1DGC_HCE_AFR_FINAL_sample_list.txt"
+covarFile="T1DGC_HCE_AFR_FINAL_cov.txt"
+outPath="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/logistic_reg"
+outStem="AFR_only"
 # 1KG imputed out stem:
 ## outStem="1KG.AMR_only"
 
@@ -38,7 +40,25 @@ outStem="multiethnic.AMR_only.v3"
 # get just AFR/AMR samples and adjust for population structure
 plink --vcf $filePath/${vcfFileName} --logistic hide-covar \
   --keep $filterPath/${keepFile} --covar $filterPath/${covarFile} \
-  --pheno $filterPath/${phenoFile} --mpheno 2 \
-  --out $filePath/${outStem} --allow-no-sex --double-id
+  --pheno $filterPath/${phenoFile}  --mpheno 2 \
+  --maf .01 --allow-no-sex \
+  --out $outPath/${outStem}
+
+
+#################################
+## logistic regression on EUR ###
+#################################
+filePath="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6"
+filterPath="/nv/vol185/T1DGC/USERS/cat7ep/data"
+keepFile="tmp_5_fam_EUR_cc_unrelated.txt"
+covarFile="T1DGC_HCE_EUR_FINAL_cov.txt"
+outPath="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/logistic_reg"
+outStem="EUR_only"
+
+plink --bfile $filePath/T1DGC_HCE_unrelated_EUR --logistic hide-covar \
+  --keep $filePath/${keepFile} --covar $filterPath/${covarFile} \
+  --maf .01 --allow-no-sex \
+  --out $outPath/${outStem}
 
 # go to run_HLAManhattan.sh to make plot with this data
+# go to stepwise_analysis.sh to initiate stepwise process
--- run_HLAManhattan.sh
+++ run_HLAManhattan.sh
@@ -4,9 +4,9 @@ module load  gcc/7.1.0  openmpi/3.1.4
 module load R/3.6.3
 module load anaconda
 
-inDir="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/linear_reg"
+inDir="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/logistic_reg"
 outDir="/nv/vol185/T1DGC/USERS/cat7ep/project/figures"
-stem="linear_AFR.p2"
+stem="EUR_only"
 ## stems: multiethnic.AMR_only.v4; multiethnic.AFR_only.v4
 ##        linear_AFR,p2; linear_AMR.p2
 
@@ -16,6 +16,6 @@ cd /nv/vol185/T1DGC/USERS/cat7ep/HLA-TAPAS
 python -m HLAManhattan \
     --assoc-result ${inDir}/${stem}.assoc.logistic \
     --hg 19 \
-    --out ${outDir}/linear_AFR.Manhattan
+    --out ${outDir}/logistic_EUR.Manhattan
 
 ## scp pdf files to see them
