68d92d2b746d0cdc30c71d435982c5c7edde8ead
Courtney Tern <catern19@gmail.com>
2022-04-01 00:31:47 -0400

    rename

--- filterVCF_makeBinary.sh
+++ filterVCF_makeBinary.sh
@@ -4,18 +4,17 @@
 ## This file will filter the imputed VCF file to exclude any sites with low Rsq values and create a new VCF
 ## Then, it will use the new VCF file to create the appropriate binary files
 
-module load vcftools
 module load plink
 
 wd="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6"
 vcfFileName="chr6.dose.vcf.gz"
-excludeFile="lowRsqSNPs.txt"
+excludeFile="filteredOutSNPs.txt"
 
 cd $wd
 
-vcftools --gzvcf ${vcfFileName} --exclude ${excludeFile} --recode --out filtered021322
+plink --vcf ${vcfFileName} --exclude ${excludeFile} --recode vcf --out filtered032722
 
 #### check file paths when vcftools finishes running
-plink --vcf $wd/filtered021322.recode.vcf.gz --make-bed --out filtered021322
+plink --vcf $wd/filtered032722.vcf.gz --make-bed --out filtered032722
 
-# now go to run_HLAOmnibus.sh
+# make summary table in make_allele_summary.sh
