47b52c801d076f40ceefda8d4cfbc8460411877c
Courtney Tern <catern19@gmail.com>
2022-04-15 22:56:35 -0400

    housekeeping

--- misc/makeAMR-AFR-EUR_VCF_binary.sh
+++ misc/makeAMR-AFR-EUR_VCF_binary.sh
@@ -6,6 +6,7 @@
 module load plink
 
 wd="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6"
+sampsDir="/nv/vol185/T1DGC/USERS/cat7ep/data"
 AFR_samps="T1DGC_HCE_AFR_FINAL_sample_list.txt"
 AMR_samps="T1DGC_HCE_AMR_FINAL_sample_list.txt"
 EUR_samps="tmp_5_fam_EUR_cc_unrelated.txt"
@@ -16,10 +17,12 @@ EUR_out="T1DGC_HCE_unrelated_EUR"
 cd $wd
 
 # make VCFs
-plink --vcf $wd/filtered021322.recode.vcf.gz \
-      --keep $wd/$AFR_samps --recode vcf --out $AFR_out
-plink --vcf $wd/filtered021322.recode.vcf.gz \
-      --keep $wd/$AMR_samps --recode vcf --out $AMR_out
+plink --vcf $wd/filtered032722.vcf.gz \
+      --keep $sampsDir/$AFR_samps --recode vcf --out $AFR_out
+plink --vcf $wd/filtered032722.vcf.gz \
+      --keep $sampsDir/$AMR_samps --recode vcf --out $AMR_out
+plink --bfile $wd/T1DGC_HCE_updated_fam \
+      --keep $wd/$EUR_samps --recode vcf --out $wd/$EUR_out
 
 # make binary files
 plink --vcf $wd/${AFR_out}.vcf --make-bed --out $AFR_out
