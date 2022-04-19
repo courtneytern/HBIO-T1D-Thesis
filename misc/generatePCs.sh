c378ecfa2542cd1d0a10aa377a3cb296fcc13cbc
Courtney Tern <catern19@gmail.com>
2022-04-19 17:46:23 -0400

    move

--- misc/generatePCs.sh
+++ misc/generatePCs.sh
@@ -9,13 +9,15 @@ cd /nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/temp
 logfile=EUR_PCs.log
 filePath="/nv/vol185/T1DGC/USERS/cat7ep/data/multiethnic_imputed/chr_6/"
 
+# split cases and controls from unrelated EUR data
 cat $filePath/T1DGC_HCE_unrelated_EUR.fam | awk '$NF ~ /1/ {print $1, $2}' > tmp_EUR_unrelated_control.txt
 cat $filePath/T1DGC_HCE_unrelated_EUR.fam | awk '$NF ~ /2/ {print $1, $2}' > tmp_EUR_unrelated_case.txt
 
+# make binary files for case and control separately
 plink --bfile $filePath/T1DGC_HCE_unrelated_EUR --keep tmp_EUR_unrelated_control.txt --make-bed --out tmp_2_EUR_unrelated_control &>> $logfile
 plink --bfile $filePath/T1DGC_HCE_unrelated_EUR --keep tmp_EUR_unrelated_case.txt --make-bed --out tmp_2_EUR_unrelated_case &>> $logfile
 
-# Projects case samples from AFR population on control samples
+# Projects case samples from population on control samples
 $KING -b tmp_2_EUR_unrelated_control.bed,tmp_2_EUR_unrelated_case.bed --pca --proj --rplot --prefix tmp_EUR_unrelated_cc &>> $logfile
 
 
